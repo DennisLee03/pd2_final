0290bd2e155c9e1c2466cc542f894e5ef1068203
DennisLee03 <e64112287@gs.ncku.edu.tw>
2024-06-17 00:05:33 +0800

    delete redundant import

--- src/main/java/com/kfccoupon/pd2/controller/MainController.java
+++ src/main/java/com/kfccoupon/pd2/controller/MainController.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kfccoupon.pd2.dto.CouponMealDto;
 import com.kfccoupon.pd2.service.CouponService;
 import com.kfccoupon.pd2.service.MongoDeleteCollection;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/main")
