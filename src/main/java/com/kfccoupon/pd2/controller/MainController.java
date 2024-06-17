1cc2dd373b4a80a896e21c21212e3c6586a7785c
DennisLee03 <e64112287@gs.ncku.edu.tw>
2024-06-17 08:44:02 +0800

    adjust service and dao

--- src/main/java/com/kfccoupon/pd2/controller/MainController.java
+++ src/main/java/com/kfccoupon/pd2/controller/MainController.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
